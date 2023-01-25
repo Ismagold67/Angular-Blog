4f2edba1d7cc83e649dcb066aa547b09eacd1dc9
IsmaGold67 <ismaelroberto67@gmail.com>
2023-01-24 23:22:39 -0400

    components createds with style

---
+++ src/app/components/men-title/men-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-men-title',
+  templateUrl: './men-title.component.html',
+  styleUrls: ['./men-title.component.css']
+})
+export class MenTitleComponent {
+
+}
