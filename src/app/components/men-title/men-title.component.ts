557248bc29b5e6fc7319ac414c0cce2ebcfe669d
IsmaGold67 <ismaelroberto67@gmail.com>
2023-01-25 20:58:21 -0400

    components dinamycs propertys

--- src/app/components/men-title/men-title.component.ts
+++ src/app/components/men-title/men-title.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-men-title',
   templateUrl: './men-title.component.html',
-  styleUrls: ['./men-title.component.css']
+  styleUrls: ['./men-title.component.css', './men-title.responsive.component.css']
 })
 export class MenTitleComponent {
 
