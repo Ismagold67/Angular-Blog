d7be0fc1ad5c3200998ef14f69216208ad157541
IsmaGold67 <ismaelroberto67@gmail.com>
2023-01-26 13:25:41 -0400

    new version created

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,15 +1,27 @@
 export const dataFake = [
     {
+        "id":'0',
+        "title":"O QUE ESPERAR DE HOMEM FORMIGA E A VESPA: QUANTUMANIA",
+        "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam reprehenderit natus ab esse iusto, fugiat voluptatem, ullam excepturi amet eaque sit veritatis similique provident? Aut incidunt officiis accusantium eum. Nesciunt?Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam reprehenderit natus ab esse iusto, fugiat voluptatem, ullam excepturi amet eaque sit veritatis similique provident? Aut incidunt officiis accusantium eum. Nesciunt?Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam reprehenderit natus ab esse iusto, fugiat voluptatem, ullam excepturi amet eaque sit veritatis similique provident? Aut incidunt officiis accusantium eum. Nesciunt?",
+        "photoCover":"https://i0.wp.com/www.portaldonerd.com.br/wp-content/uploads/2023/01/cropped-1600-900-1298672-1.jpg"
+    },
+    {
         "id":'1',
-        "title":"novo quadrinho anunciado",
-        "description":"ola",
-        "photoCover":"https://aiptcomics.com/wp-content/uploads/2019/04/knightscover-min-e1556678390424.jpg"
+        "title":"REVELADO EM QUE ÉPOCA SE PASSARÁ O NOVO FILME DO BLADE",
+        "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam reprehenderit natus ab esse iusto, fugiat voluptatem, ullam excepturi amet eaque sit veritatis similique provident? Aut incidunt officiis accusantium eum. Nesciunt?Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam reprehenderit natus ab esse iusto, fugiat voluptatem, ullam excepturi amet eaque sit veritatis similique provident? Aut incidunt officiis accusantium eum. Nesciunt?Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam reprehenderit natus ab esse iusto, fugiat voluptatem, ullam excepturi amet eaque sit veritatis similique provident? Aut incidunt officiis accusantium eum. Nesciunt?",
+        "photoCover":"https://pop.proddigital.com.br/wp-content/uploads/sites/8/2022/11/blade-na-marvel-veja-o-que-muda-para-o-personagem-divulgacao.jpg"
     },
     {
         "id":'2',
         "photo":"aaa",
-        "title":"O valor de uma reqlíquia",
-        "description":"Não é de muito tempo que....",
-        "photoCover":"https://1.bp.blogspot.com/-w113gepdP7U/YE09gp6H-fI/AAAAAAACKWs/MX5Y1ix1koUViTFssxZ80RxjEv6D3NldACLcBGAsYHQ/s540/raro0.jpg"
+        "title":"CONFIRA TODAS AS ABERTURAS DOS FILMES DA MARVEL",
+        "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam reprehenderit natus ab esse iusto, fugiat voluptatem, ullam excepturi amet eaque sit veritatis similique provident? Aut incidunt officiis accusantium eum. Nesciunt?Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam reprehenderit natus ab esse iusto, fugiat voluptatem, ullam excepturi amet eaque sit veritatis similique provident? Aut incidunt officiis accusantium eum. Nesciunt?Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam reprehenderit natus ab esse iusto, fugiat voluptatem, ullam excepturi amet eaque sit veritatis similique provident? Aut incidunt officiis accusantium eum. Nesciunt?",
+        "photoCover":"https://i.pinimg.com/originals/e3/56/37/e35637dd0c23169096c7e6bc574d39bc.gif"
+    },
+    {
+        "id":'3',
+        "title":"ATOR DE VINGADORES SOFRE GRAVE ACIDENTE",
+        "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam reprehenderit natus ab esse iusto, fugiat voluptatem, ullam excepturi amet eaque sit veritatis similique provident? Aut incidunt officiis accusantium eum. Nesciunt?Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam reprehenderit natus ab esse iusto, fugiat voluptatem, ullam excepturi amet eaque sit veritatis similique provident? Aut incidunt officiis accusantium eum. Nesciunt?Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam reprehenderit natus ab esse iusto, fugiat voluptatem, ullam excepturi amet eaque sit veritatis similique provident? Aut incidunt officiis accusantium eum. Nesciunt?",
+        "photoCover":"https://img.olhardigital.com.br/wp-content/uploads/2023/01/Jeremy-Renner.png"
     }
 ]
